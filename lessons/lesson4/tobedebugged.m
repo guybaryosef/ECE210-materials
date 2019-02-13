2c654dc9c401239dc2d02bd54412fcadda73aeb9
Guy Bar Yosef <guybaryosef@gmail.com>
2019-02-13 13:42:45 -0500

    some last minute final touches

--- lessons/lesson4/tobedebugged.m
+++ lessons/lesson4/tobedebugged.m
@@ -1,13 +1,19 @@
 A = rand(5,3);
 y = f(A);
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%% supporting functions %%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function y = f(A)
+    % This function takes in a matrix A and returns the matrix as one long
+    % row vector.
+    
     y = [];
-    N = size(A,2);
-    M = size(A,1);
-    for n = 1:N
-        for m = 1:M
-            y = [y A(n,m)];
+    R = size(A,2);
+    C = size(A,1);
+    for i = 1:R
+        for j = 1:C
+            y = [y A(i,j)];
         end
     end
 end
