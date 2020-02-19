90db8975b916c488eded7b160ab03ac9d92779ff
Guy Bar Yosef <guybaryosef@gmail.com>
2020-02-19 00:53:01 -0500

    ready for week 4

---
+++ lessons/lesson4/tobedebugged.m
@@ -0,0 +1,24 @@
+% Fancy assignment 
+% Some fancy class.
+% By: Yours truly.
+
+%% Part 1 
+A = rand(5,3);
+y = f(A);
+
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%% supporting functions %%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function y = f(A)
+    % This function takes in a matrix A and returns the matrix as one long
+    % row vector.
+    
+    y = [];
+    R = size(A,2);
+    C = size(A,1);
+    for i = 1:R
+        for j = 1:C
+            y = [y A(i,j)];
+        end
+    end
+end
